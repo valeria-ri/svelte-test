e5cc651c20df8a0248eee3ba8727d9a34fd4db9f
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-04-21 16:16:06 +0300

    feat: решить первую задачу

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,39 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const immutableKeys = ['groupId', 'service', 'formatSize', 'ca'];
 
+const decodeObj = (obj, translations, exceptions, uniqueSet) => {
+  let newObj = {};
+  for (const key in obj) {
+    if (exceptions.includes(key) || obj[key] == null) {
+      newObj[key] = obj[key];
+    }
+    else if (!Object.keys(translations).includes(obj[key])) {
+      uniqueSet.add(obj[key]);
+      newObj[key] = obj[key];
+    }
+    else {
+      newObj[key] = translations[obj[key]];
+    }
+  }
+  return newObj;
+};
 
+const decodeData = (arr, translations, exceptions) => {
+  let uniqueValues = new Set();
+  const decoded = arr.map(i => decodeObj(
+    i,
+    translations,
+    exceptions,
+    uniqueValues,
+  ));
+  return { 
+    decoded, 
+    uniqueValues: Array.from(uniqueValues)
+  };
+};
 
-// console.log(decoded)
+const { decoded, uniqueValues } = decodeData(encoded, translations, immutableKeys);
+
+console.log('Расшифрованные данные:', decoded);
+console.log('Уникальные id:', uniqueValues);
