6d5825126155a9dc56ef08ef9d0ca05216f07b01
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-04-22 04:44:09 +0300

    feat: реализовать функционал конвертера

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,114 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  const key: string = "e0304165b5989eb12ecbd489";
+
+  const number1name: string = "value1";
+  const number2name: string = "value2";
+  const currency1name: string = "currency1";
+  const currency2name: string = "currency2";
+
+  let currency1: string = "RUB";
+  let currency2: string = "USD";
+  let value1: number = 1;
+  let value2: number;
+  let supportedCurrencies: string[] = [];
+
+  onMount(async () => {
+    const res = await fetch(
+      `https://v6.exchangerate-api.com/v6/${key}/codes`,
+      );
+    const data = await res.json();
+    supportedCurrencies = await data.supported_codes;
+    initConversion();
+  });
+
+  async function getCurrencyData(a: string, b: string) {
+    const res = await fetch(
+      `https://v6.exchangerate-api.com/v6/${key}/pair/${a}/${b}`,
+    );
+    const data = await res.json();
+    return await parseFloat(data.conversion_rate.toFixed(4));
+  }
+
+  async function convert(a: string, b: string, value: number): Promise<number> {
+    const converted = await getCurrencyData(a, b);
+    return value * converted;
+  }
+
+  function initConversion() {
+    convert(currency1, currency2, value1).then((res) => (value2 = res));
+  }
+
+  function changeValues(event: Event) {
+    const value = parseFloat((event.target as HTMLInputElement).value) || 0;
+    const name = (event.target as HTMLInputElement).name;
+
+    if (name === number1name) {
+      convert(currency1, currency2, value).then((res) => (value2 = res));
+    }
+    if (name === number2name) {
+      convert(currency2, currency1, value).then((res) => (value1 = res));
+    }
+  }
+
+  function changeCurrencies(event: Event) {
+    const { value, name } = event.target as HTMLInputElement;
+    if (name === currency1name) {
+      convert(value, currency2, value1).then((res) => (value2 = res));
+    }
+    if (name === currency2name) {
+      convert(currency1, value, value1).then((res) => (value2 = res));
+    }
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="page">
+  <h1 class="title">Currency converter</h1>
+  <section class="converter">
+    <fieldset class="converter__field">
+      <input
+        class="converter__input"
+        type="number"
+        name={number1name}
+        bind:value={value1}
+        on:input={changeValues}
+        min="0"
+      />
+      <select
+        class="converter__select"
+        name={currency1name}
+        bind:value={currency1}
+        on:change={changeCurrencies}
+      >
+        {#each supportedCurrencies as currency}
+          <option value={currency[0]}>
+            {currency[1]}
+          </option>
+        {/each}
+      </select>
+    </fieldset>
+    <fieldset class="converter__field">
+      <input
+        class="converter__input"
+        type="number"
+        name={number2name}
+        bind:value={value2}
+        on:input={changeValues}
+        min="0"
+      />
+      <select
+        class="converter__select"
+        name={currency2name}
+        bind:value={currency2}
+        on:change={changeCurrencies}
+      >
+        {#each supportedCurrencies as currency}
+          <option value={currency[0]}>
+            {currency[1]}
+          </option>
+        {/each}
+      </select>
+    </fieldset>
+  </section>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
